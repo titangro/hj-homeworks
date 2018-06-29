7cf970324bf444d3755b3d9aa2f180cac025b403
Roman <titangro@users.noreply.github.com>
2018-06-29 16:26:26 +0300

    Выполнено задание gallery

---
+++ browser/gallery/js/script.js
@@ -0,0 +1,28 @@
+'use strict';
+const images = ['breuer-building.jpg','guggenheim-museum.jpg','headquarters.jpg','IAC.jpg','new-museum.jpg'];
+let i = 0;
+function changeImage() {
+	const currentImage = document.getElementById('currentPhoto');
+	const currentImageName = currentImage.src.slice(currentImage.src.search(/\/i\//g)+3);	
+	if (this.id === 'prevPhoto') {			
+		if (i > 0) {
+			i--;					
+			currentImage.src = './i/' + images[i];
+		} else {
+			i = images.length - 1;
+			currentImage.src = './i/' + images[i];
+		}
+	} else {		
+		if (i < images.length - 1) {
+			i++;
+			currentImage.src = './i/' + images[i];
+		} else {
+			i = 0;
+			currentImage.src = './i/' + images[i];				
+		}
+	}
+}
+for (const btnId of ['prevPhoto', 'nextPhoto']) {
+	const btn = document.getElementById(btnId);
+	btn.onclick = changeImage;
+}
