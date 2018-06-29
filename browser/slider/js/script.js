89da2eabb311e59988fabfcbc7db57a022109526
Roman <titangro@users.noreply.github.com>
2018-06-29 13:21:33 +0300

    Выполнено задание errors

--- browser/slider/js/script.js
+++ browser/slider/js/script.js
@@ -1,15 +1,16 @@
-const image = document.getElementById("slider");
+'use strict';
+const image = document.getElementById('slider');
 setInterval(function() {
 	let currentImg = image.src.slice(image.src.search(/\/i\//g));
-	if (currentImg === "/i/airmax-jump.png") {
-		image.src = "./i/airmax-on-foot.png";
-	} else if (currentImg === "/i/airmax-on-foot.png") {
-		image.src = "./i/airmax-playground.png";
-	} else if (currentImg === "/i/airmax-playground.png") {
-		image.src = "./i/airmax-top-view.png";
-	} else if (currentImg === "/i/airmax-top-view.png") {
-		image.src = "./i/airmax.png";
+	if (currentImg === '/i/airmax-jump.png') {
+		image.src = './i/airmax-on-foot.png';
+	} else if (currentImg === '/i/airmax-on-foot.png') {
+		image.src = './i/airmax-playground.png';
+	} else if (currentImg === '/i/airmax-playground.png') {
+		image.src = './i/airmax-top-view.png';
+	} else if (currentImg === '/i/airmax-top-view.png') {
+		image.src = './i/airmax.png';
 	} else {
-		image.src = "./i/airmax-jump.png";
+		image.src = './i/airmax-jump.png';
 	}
 }, 5000);
