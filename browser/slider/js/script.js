77a109e9f48a1a047b89e69cfe1abbe527f395be
Roman <titangro@users.noreply.github.com>
2018-06-29 13:03:18 +0300

    Выполнено задание slider

---
+++ browser/slider/js/script.js
@@ -0,0 +1,15 @@
+const image = document.getElementById("slider");
+setInterval(function() {
+	let currentImg = image.src.slice(image.src.search(/\/i\//g));
+	if (currentImg === "/i/airmax-jump.png") {
+		image.src = "./i/airmax-on-foot.png";
+	} else if (currentImg === "/i/airmax-on-foot.png") {
+		image.src = "./i/airmax-playground.png";
+	} else if (currentImg === "/i/airmax-playground.png") {
+		image.src = "./i/airmax-top-view.png";
+	} else if (currentImg === "/i/airmax-top-view.png") {
+		image.src = "./i/airmax.png";
+	} else {
+		image.src = "./i/airmax-jump.png";
+	}
+}, 5000);
