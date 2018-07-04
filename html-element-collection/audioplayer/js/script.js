f5699cd5bf382895fdd340225f9bebf8031befb7
Roman <titangro@users.noreply.github.com>
2018-07-04 21:52:58 +0300

    Упростил код, убрал обращения на иконки

--- html-element-collection/audioplayer/js/script.js
+++ html-element-collection/audioplayer/js/script.js
@@ -2,8 +2,6 @@
 const media = document.getElementsByClassName('mediaplayer')[0],
 player = document.getElementsByClassName('player')[0],
 playButton = document.getElementsByClassName('playstate')[0],
-playIcon = document.getElementsByClassName('fa-play')[0],
-puaseIcon = document.getElementsByClassName('fa-pause')[0],
 stopButton = document.getElementsByClassName('stop')[0],
 backButton = document.getElementsByClassName('back')[0],
 nextButton = document.getElementsByClassName('next')[0],
@@ -14,21 +12,15 @@ mediaList = [{title: 'LA Chill Tour', src: 'https://netology-code.github.io/hj-h
 let songIndex = 0;
 playButton.onclick = () => {	
 	if (player.paused) {
-		player.play();
-		playIcon.style.display = "none";
-		puaseIcon.style.display = "block";		
+		player.play();				
 		media.classList.add('play');
 	} else {
-		player.pause();
-		playIcon.style.display = "block";
-		puaseIcon.style.display = "none";		
+		player.pause();				
 		media.classList.remove('play');
 	}
 };
 stopButton.onclick = () => {	
-	player.currentTime = 0;
-	playIcon.style.display = "block";
-	puaseIcon.style.display = "none";
+	player.currentTime = 0;	
 	player.pause();		
 	media.classList.remove('play');
 };
