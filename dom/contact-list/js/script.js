5a3eb1467be18dd22b718d2550d96766b8e486f7
Roman <titangro@users.noreply.github.com>
2018-07-11 21:35:35 +0300

    Выполнено задание список контактов

---
+++ dom/contact-list/js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+let contactsPole = document.querySelector('.contacts-list');
+const newContact = document.querySelector('.contacts-list li');
+contactsPole.innerHTML = "";
+const constactsList = JSON.parse(loadContacts());
+for (let contact of constactsList) {
+	newContact.dataset.email = contact.email;
+	newContact.dataset.phone = contact.email;
+	newContact.innerHTML = "<strong>" + contact.name + "</strong>";	
+	contactsPole.innerHTML += newContact.outerHTML;
+}
