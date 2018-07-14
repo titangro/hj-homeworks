0b2add3325faf6e42668e20ee152be19b2540683
Roman <titangro@users.noreply.github.com>
2018-07-14 12:03:03 +0300

    Поправил задание Tabs

--- xhr/tabs/js/script.js
+++ xhr/tabs/js/script.js
@@ -5,14 +5,23 @@ const xhrEmail = new XMLHttpRequest(),
 	smsButton = document.querySelector('.sms'),
 	shower = document.getElementById('content'),
 	hrefEmail = emailButton.getAttribute('href'),
-	hrefSms = smsButton.getAttribute('href');
+	hrefSms = smsButton.getAttribute('href'),
+	loading = document.getElementById('preloader');
+
 xhrEmail.open('GET', hrefEmail, true);
 xhrEmail.send();
 xhrSms.open('GET', hrefSms, true);
-xhrSms.send();	
+xhrSms.send();
+
 xhrEmail.addEventListener('readystatechange', onReadyStateChange);
 emailButton.addEventListener('click', onClickChangeToEmail);
 smsButton.addEventListener('click', onClickChangeToSms);
+xhrEmail.addEventListenr('loadstart', onLoadStart);
+xhrEmail.addEventListenr('loadend', onLoadEnd);
+emailButton.addEventListenr('loadstart', onLoadStart);
+emailButton.addEventListenr('loadend', onLoadEnd);
+smsButton.addEventListenr('loadstart', onLoadStart);
+smsButton.addEventListenr('loadend', onLoadEnd);
 
 function onReadyStateChange(event) {
 	if (event.target.readyState != 4) {
@@ -22,13 +31,27 @@ function onReadyStateChange(event) {
 	 	shower.innerHTML = event.target.responseText;
 	}	
 }
+
 function onClickChangeToEmail(event) {
 	event.preventDefault();
 	xhrEmail.open('GET', hrefEmail, true);
-	xhrEmail.send();	
+	xhrEmail.send();
+	emailButton.classList.add('active');
+	smsButton.classList.remove('active');
 }
+
 function onClickChangeToSms(event) {
 	event.preventDefault();
-	shower.innerHTML = xhrSms.responseText;	
+	shower.innerHTML = xhrSms.responseText;
+	smsButton.classList.add('active');
+	emailButton.classList.remove('active');
+}
+
+function onLoadStart() {
+	loading.classList.remove('hidden');
+}
+
+function onLoadEnd() {
+	loading.classList.add('hidden');
 }
 
