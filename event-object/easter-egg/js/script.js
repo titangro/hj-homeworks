3cd98ca8dfb6a0e4d9a568a43aa43ebbf18873c9
Roman <titangro@users.noreply.github.com>
2018-07-28 14:40:14 +0300

    Изменил задание
    Теперь панель открывается/закрывается при нажатии; секретный код появляется только при правильном наборе кода, при этом при любом неверно введенном символе код нужно вводить заново

--- event-object/easter-egg/js/script.js
+++ event-object/easter-egg/js/script.js
@@ -1,35 +1,24 @@
 'use scrict';
+const nav = document.getElementsByTagName('nav')[0];
 function showPanel(event) {
-	if (!event.ctrlKey) {
-		return;
+	if (event.ctrlKey && event.altKey && event.code === 'KeyT' && !event.repeat) {
+		nav.classList.toggle('visible');
 	}
-	if (!event.altKey) {
-		return;
-	}
-	switch (event.code) {
-		case 'KeyT':
-			document.getElementsByTagName('nav')[0].classList.add('visible');
-		break;				
-	}
- 	
 }
-let key = "";
-if (key === "") {
+let key = [];
+if (Array.isArray(key)) {
 	console.log('Введите секретный код');
 }
 function showSecret(event) {	
-	key += event.key;
-	if (key === 'ytnjkjubz') {
-		document.addEventListener('keyup', () => document.getElementsByClassName('secret')[0].classList.add('visible'));	
-		key = "";
-	} else {
-		if (key.length > 8) {
-			key = "";
-			console.log('Попробуйте ввести секретный код заново')
-		}
-		document.addEventListener('keyup', () => document.getElementsByClassName('secret')[0].classList.remove('visible'));
+	key.push(event.code);
+	console.log(key)
+	if (key[0] === 'KeyY' && key[1] === 'KeyT' && key[2] === 'KeyN' && key[3] === 'KeyJ' && key[4] === 'KeyK' && key[5] === 'KeyJ' && key[6] === 'KeyU' && key[7] === 'KeyB' && key[8] === 'KeyZ') {
+		document.getElementsByClassName('secret')[0].classList.toggle('visible');
 	}
+	if (key.length > 8 || key[0] !== 'KeyY' && !!key[0] || key[1] !== 'KeyT' && !!key[1] || key[2] !== 'KeyN' && !!key[2] || key[3] !== 'KeyJ' && !!key[3] || key[4] !== 'KeyK' && !!key[4] || key[5] !== 'KeyJ' && !!key[5] || key[6] !== 'KeyU' && !!key[6] || key[7] !== 'KeyB' && !!key[7] || key[8] !== 'KeyZ' && !!key[8]) {
+		console.log('Попробуйте ввести секретный код заново');
+		key = [];
+	}	
 }
 document.addEventListener('keydown', showPanel);
-document.addEventListener('keyup', () => document.getElementsByTagName('nav')[0].classList.remove('visible'));
 document.addEventListener('keydown', showSecret);
