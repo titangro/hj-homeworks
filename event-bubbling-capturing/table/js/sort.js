5804f0ff49e03db15e2c8df83fb6496062616131
Roman <titangro@users.noreply.github.com>
2018-07-30 22:13:18 +0300

    Выполнено задание Сортировка таблицы

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,14 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  if (event.target.nodeName === 'TH') {
+  	if (event.target.dataset.dir != 1) {
+  		event.target.dataset.dir = 1;
+  	} else {
+  		event.target.dataset.dir = -1;
+  	}
+  	const tableSort = document.querySelector('table');
+  	tableSort.dataset.sortBy = event.target.dataset.propName;
+  	sortTable(tableSort.dataset.sortBy, event.target.dataset.dir);
+  }
 }
