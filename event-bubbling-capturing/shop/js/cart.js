6443db623522ed609ab7752b8ca1e36f2ecfa28d
Roman <titangro@users.noreply.github.com>
2018-07-30 22:31:21 +0300

    Выполнено задание Добавить в корзину

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,11 @@
 'use strict';
+
+document.querySelector('.items-list').addEventListener('click', event => {
+	event.preventDefault();
+	if (event.target.classList.contains('add-to-cart')) {
+		addToCart({
+				title: event.target.dataset.title,
+			 	price: event.target.dataset.price
+			});
+	}
+})
