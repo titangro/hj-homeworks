0641e7117b9bd80c071e3257588f9d3e3ba19935
Roman <titangro@users.noreply.github.com>
2018-08-20 23:47:57 +0300

    Задание  sign-in-form

--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -1,1 +1,55 @@
 'use strict';
+const singInForm = document.querySelector('.sign-in-htm'),
+	singUpForm = document.querySelector('.sign-up-htm');
+
+const formDataSingIn = new FormData(singInForm);
+const formDataSingUp = new FormData(singUpForm);
+for (const [key, value] of formDataSingIn) {
+	console.log(key + ': ' + value)
+}
+
+const requestSingIn = fetch('https://neto-api.herokuapp.com/signin', {
+	body: JSON.stringify({formDataSingIn}),
+	credentials: 'same-origin',
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json'
+	}
+})
+	.then((res) => {
+		if (200 <= res.status && res.status < 300) {
+			return res;
+		}
+		throw new Error(response.statusText);
+	})
+	.then((res) => { return res.json(); })
+	.then((data) => {
+		singInForm.querySelector('.error-message').textContent = `Пользователь ${data.name} успешно авторизован`;
+	})
+	.catch((error) => {
+		console.log(error, error.error)
+		singInForm.querySelector('.error-message').textContent = error.message;
+	});
+
+const requestSingUp = fetch('https://neto-api.herokuapp.com/signup', {
+	body: JSON.stringify({formDataSingUp}),
+	credentials: 'same-origin',
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json'
+	}
+})
+	.then((res) => {
+		if (200 <= res.status && res.status < 300) {
+			return res;
+		}
+		throw new Error(response.statusText);
+	})
+	.then((res) => { return res.json(); })
+	.then((data) => {
+		singUpForm.querySelector('.error-message').textContent = `Пользователь ${data.name} успешно зарегистрирован`;
+	})
+	.catch((error) => {
+		console.log(error, error.error)
+		singUpForm.querySelector('.error-message').textContent = error.message;
+	});
