e4ccc2346f3326b2a0b8306a5016ca2a024f308f
Roman <titangro@users.noreply.github.com>
2018-10-30 22:26:10 +0300

    Усовершенствовал решение задания Пасхалка

--- event-object/easter-egg/js/script.js
+++ event-object/easter-egg/js/script.js
@@ -5,20 +5,29 @@ function showPanel(event) {
 		nav.classList.toggle('visible');
 	}
 }
-let key = [];
+let key = [],
+	secretKey = ['KeyY', 'KeyT', 'KeyN', 'KeyJ', 'KeyK', 'KeyJ', 'KeyU', 'KeyB', 'KeyZ'];
 if (Array.isArray(key)) {
 	console.log('Введите секретный код');
 }
 function showSecret(event) {	
-	key.push(event.code);
-	console.log(key)
-	if (key[0] === 'KeyY' && key[1] === 'KeyT' && key[2] === 'KeyN' && key[3] === 'KeyJ' && key[4] === 'KeyK' && key[5] === 'KeyJ' && key[6] === 'KeyU' && key[7] === 'KeyB' && key[8] === 'KeyZ') {
-		document.getElementsByClassName('secret')[0].classList.toggle('visible');
-	}
-	if (key.length > 8 || key[0] !== 'KeyY' && !!key[0] || key[1] !== 'KeyT' && !!key[1] || key[2] !== 'KeyN' && !!key[2] || key[3] !== 'KeyJ' && !!key[3] || key[4] !== 'KeyK' && !!key[4] || key[5] !== 'KeyJ' && !!key[5] || key[6] !== 'KeyU' && !!key[6] || key[7] !== 'KeyB' && !!key[7] || key[8] !== 'KeyZ' && !!key[8]) {
+	key.push(event.code);	
+	if (isSecret()) {
+		if (key.length === secretKey.length) {
+			document.getElementsByClassName('secret')[0].classList.toggle('visible');
+			key = [];
+		}
+	} else {
 		console.log('Попробуйте ввести секретный код заново');
 		key = [];
-	}	
+	}
 }
+
+function isSecret() {
+	for (let i = 0; i < secretKey.length; i++) {
+		return key[i] === secretKey[i]; 	
+	}
+}
+
 document.addEventListener('keydown', showPanel);
 document.addEventListener('keydown', showSecret);
