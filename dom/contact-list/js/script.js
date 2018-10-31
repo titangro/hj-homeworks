df531b21ff42ebabc94dc3a55682c54ed09b6dfb
Roman <titangro@users.noreply.github.com>
2018-10-31 23:42:53 +0300

    update

--- dom/contact-list/js/script.js
+++ dom/contact-list/js/script.js
@@ -3,7 +3,7 @@ let contactsPole = document.querySelector('.contacts-list');
 const newContact = document.querySelector('.contacts-list li');
 contactsPole.innerHTML = "";
 const constactsList = JSON.parse(loadContacts());
-for (let contact of constactsList) {
+for (let contact of constactsList) {	
 	newContact.dataset.email = contact.email;
 	newContact.dataset.phone = contact.phone;
 	newContact.innerHTML = "<strong>" + contact.name + "</strong>";	
