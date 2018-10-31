ab518f15b993dfb66328a1e71a55fc3be02213a3
Roman <titangro@users.noreply.github.com>
2018-11-01 01:11:38 +0300

    update задания Табы

--- xhr/tabs/js/script.js
+++ xhr/tabs/js/script.js
@@ -1,57 +1,44 @@
 'use strict';
-const xhrEmail = new XMLHttpRequest(),
-	xhrSms = new XMLHttpRequest(),
-	emailButton = document.querySelector('.email'),
-	smsButton = document.querySelector('.sms'),
+const xhr = new XMLHttpRequest(),
+	buttons = document.querySelectorAll('a'),
 	shower = document.getElementById('content'),
-	hrefEmail = emailButton.getAttribute('href'),
-	hrefSms = smsButton.getAttribute('href'),
 	loading = document.getElementById('preloader');
 
-xhrEmail.open('GET', hrefEmail, true);
-xhrEmail.send();
-xhrSms.open('GET', hrefSms, true);
-xhrSms.send();
+openXhr(document.querySelector('.email').getAttribute('href'));
 
-xhrEmail.addEventListener('readystatechange', onReadyStateChange);
-emailButton.addEventListener('click', onClickChangeToEmail);
-smsButton.addEventListener('click', onClickChangeToSms);
-xhrEmail.addEventListenr('loadstart', onLoadStart);
-xhrEmail.addEventListenr('loadend', onLoadEnd);
-emailButton.addEventListenr('loadstart', onLoadStart);
-emailButton.addEventListenr('loadend', onLoadEnd);
-smsButton.addEventListenr('loadstart', onLoadStart);
-smsButton.addEventListenr('loadend', onLoadEnd);
+xhr.addEventListener('load', onLoad);
+xhr.addEventListener('loadstart', onLoadStart);
+xhr.addEventListener('loadend', onLoadEnd);
 
-function onReadyStateChange(event) {
-	if (event.target.readyState != 4) {
-		return;
-	}
-	if (event.target.status === 200) {
-	 	shower.innerHTML = event.target.responseText;
-	}	
+Array.from(buttons).forEach( item => {
+	item.addEventListener('click', changeShower)
+});
+
+function openXhr(href) {
+	xhr.open('GET', href, true);
+	xhr.send();
 }
 
-function onClickChangeToEmail(event) {
-	event.preventDefault();
-	xhrEmail.open('GET', hrefEmail, true);
-	xhrEmail.send();
-	emailButton.classList.add('active');
-	smsButton.classList.remove('active');
+function onLoad(event) {
+	let { responseText } = event.target;
+	shower.innerHTML = responseText;
 }
 
-function onClickChangeToSms(event) {
+function changeShower(event) {
 	event.preventDefault();
-	shower.innerHTML = xhrSms.responseText;
-	smsButton.classList.add('active');
-	emailButton.classList.remove('active');
+	openXhr(event.target.href);
+	Array.from(buttons).forEach( item => {
+		item.classList.remove('active');
+	});
+	event.target.classList.add('active');
 }
 
 function onLoadStart() {
+	console.log(1)
 	loading.classList.remove('hidden');
 }
 
 function onLoadEnd() {
-	loading.classList.add('hidden');
+	setTimeout(50);
+	setInterval(() => {loading.classList.add('hidden')}, 1000) ;
 }
-
