8ec1168f2df05dcdbfe0975a141a210adb832949
Roman <titangro@users.noreply.github.com>
2018-10-31 23:43:07 +0300

    update Виджет погоды

--- xhr/weather-widget/js/main.js
+++ xhr/weather-widget/js/main.js
@@ -1,15 +1,11 @@
 const request = new XMLHttpRequest();
 request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather', true);
 request.send();
-request.addEventListener("readystatechange", onReadyStateChange);
+request.addEventListener("load", onLoad);
 
-function onReadyStateChange() {
-	if (request.readyState != 4) {
-		return;
-	}
-	if (request.status === 200) {
-	  const response = JSON.parse(request.responseText);
-	  setData(response);
-	}	
+function onLoad(event) {
+	let { status } = event.target;
+	console.log(status)
+	const response = JSON.parse(request.responseText);
+	setData(response);		
 }
-
