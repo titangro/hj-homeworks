e983b9b695e7914099b11f0bad5b738b143c3e86
Roman <titangro@users.noreply.github.com>
2018-11-14 01:47:29 +0300

    update

--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -2,38 +2,48 @@
 const singInForm = document.querySelector('.sign-in-htm'),
 	singUpForm = document.querySelector('.sign-up-htm');
 
-singInForm.addEventListener('submit', () => {
+singUpForm.addEventListener('submit', singUp);
+singInForm.addEventListener('submit', singIn);
+
+function singIn(event) {
+	event.preventDefault();
 	const formDataSingIn = new FormData(singInForm);	
 	fetch('https://neto-api.herokuapp.com/signin', {
-		body: JSON.stringify({formDataSingIn}),
+		body: JSON.stringify(formDataSingIn),
 		credentials: 'same-origin',
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' }
 	})
 		.then((res) => {
 			if (200 <= res.status && res.status < 300) {
-				console.log(res);
+				console.log(res)			
 				return res;
 			}
 			throw new Error(response.statusText);
 		})
-		.then((res) => { return JSON.parse(res); })
+		.then((res) => { return res.json(); })
 		.then((data) => {
+			if (data.error) {
+				throw new Error(data.message);
+			}
 			singInForm.querySelector('.error-message').textContent = `Пользователь ${data.name} успешно авторизован`;
 		})
 		.catch((error) => {
 			console.log(error, error.error);
 			singInForm.querySelector('.error-message').textContent = error.message;
 		});
-});
+};
 
-singUpForm.addEventListener('submit', () => {
+function singUp(event) {
+	event.preventDefault();
 	const formDataSingUp = new FormData(singUpForm);
+	const form = new FormData()
 	for (const [key, value] of formDataSingUp) {
-		console.log(key + ': ' + value);
+		form.append('key', 'value');
 	}
+	console.log(form)
 	fetch('https://neto-api.herokuapp.com/signup', {
-		body: JSON.stringify({formDataSingUp}),
+		body: JSON.stringify(form),
 		credentials: 'same-origin',
 		method: 'POST',
 		headers: {
@@ -42,16 +52,20 @@ singUpForm.addEventListener('submit', () => {
 	})
 		.then((res) => {
 			if (200 <= res.status && res.status < 300) {
+				console.log(res)
 				return res;
 			}
 			throw new Error(response.statusText);
 		})
-		.then((res) => { return JSON.parse(res); })
+		.then((res) => { return res.json(); })
 		.then((data) => {
+			if (data.error) {
+				throw new Error(data.message);
+			}
 			singUpForm.querySelector('.error-message').textContent = `Пользователь ${data.name} успешно зарегистрирован`;
 		})
 		.catch((error) => {
 			console.log(error, error.error);
 			singUpForm.querySelector('.error-message').textContent = error.message;
 		});
-});
+}
