3d089a0a311d042911480931240569e62d16bca5
Roman <titangro@users.noreply.github.com>
2018-11-13 21:05:38 +0300

    Переправил задание Слайдер

--- html-document-structure/slider/js/slider.js
+++ html-document-structure/slider/js/slider.js
@@ -1,47 +1,56 @@
 'use strict';
 const buttons = document.querySelectorAll('.slider-nav a');
 const sliders = document.querySelectorAll('.slides .slide');
-const next = document.querySelector('[data-action="next"]');
-const prev = document.querySelector('[data-action="prev"]');
-const first = document.querySelector('[data-action="first"]');
-const last = document.querySelector('[data-action="last"]');
-sliders[0].classList.add('slide-current');
-prev.classList.add('disabled');
-first.classList.add('disabled');
-next.addEventListener('click', event => moveSlide(true, false, event));
-last.addEventListener('click', event => moveSlide(true, true, event));
-prev.addEventListener('click', event => moveSlide(false, false, event));
-first.addEventListener('click', event => moveSlide(false, true, event));
 
-function moveSlide(isForward, toTail, event) {
+sliders[0].classList.add('slide-current');
+updateButtons();
+
+Array.from(buttons).forEach(item => {
+	item.addEventListener('click', moveSlide);
+	item.addEventListener('click', updateButtons);
+});
+
+function moveSlide(event) {
 	if (event.currentTarget.classList.contains('disabled')) return;
- 	const currentSlide = document.querySelector('.slide-current');    
-    let activatedSlide = currentSlide.nextElementSibling;    
-    if (toTail) {
-    	activatedSlide = isForward ? currentSlide.parentElement.lastElementChild : currentSlide.parentElement.firstElementChild;
-    	currentSlide.classList.remove('slide-current');
-    	activatedSlide.classList.add('slide-current');    	
-    } else {
-    	if (isForward) {
-    		activatedSlide = currentSlide.nextElementSibling;
-    	} else {
+ 	const currentSlide = document.querySelector('.slide-current');
+    let activatedSlide = currentSlide.nextElementSibling;  
+
+    switch (event.target.dataset.action) {
+    	case 'prev': 
     		activatedSlide = currentSlide.previousElementSibling;
-    	}
-    	activatedSlide.classList.add('slide-current');
-    	currentSlide.classList.remove('slide-current');    	
-    }    
-	if (activatedSlide.nextElementSibling) {
-		next.classList.remove('disabled');
-		last.classList.remove('disabled');
-	} else {
-		next.classList.add('disabled');
-		last.classList.add('disabled');
-	}
-	if (activatedSlide.previousElementSibling) {
-		prev.classList.remove('disabled');
-		first.classList.remove('disabled');
-	} else {
-		prev.classList.add('disabled');
-		first.classList.add('disabled');
-	}        	
-}   	
+    		activatedSlide.classList.add('slide-current');
+    		break;
+    	case 'first': 
+    		activatedSlide = currentSlide.parentElement.firstElementChild;
+    		activatedSlide.classList.add('slide-current');
+    		break;
+    	case 'next': 
+    		activatedSlide = currentSlide.nextElementSibling;
+    		activatedSlide.classList.add('slide-current');
+    		break;
+    	case 'last': 
+    		activatedSlide = currentSlide.parentElement.lastElementChild;
+    		activatedSlide.classList.add('slide-current');
+    		break;
+    }
+    currentSlide.classList.remove('slide-current');    
+}
+
+function updateButtons(event) {
+	const currentSlide = document.querySelector('.slide-current');
+	Array.from(buttons).forEach(item => {
+		if(item.dataset.action === 'prev' || item.dataset.action === 'first') {
+			if(!currentSlide.previousElementSibling) {
+				item.classList.add('disabled');
+			} else {
+				item.classList.remove('disabled');
+			}
+		} else {
+			if(!currentSlide.nextElementSibling) {
+				item.classList.add('disabled');
+			} else {
+				item.classList.remove('disabled');
+			}
+		}
+	});
+}
