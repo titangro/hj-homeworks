ec49da72cb7f4ef14263b486f9fde12b1c2d5d12
Roman <titangro@users.noreply.github.com>
2018-11-17 19:15:17 +0300

    Выполнил задание Счетчик открытых соединений

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/counter');
+
+connection.addEventListener('open', () => {		
+});
+
+connection.addEventListener('message', event => {	
+	let message = JSON.parse(event.data);
+	if (message.connections) {
+		document.querySelector('.counter').textContent = message.connections;
+	}
+	if (message.errors) {
+		document.querySelector('output.errors').textContent = message.errors;
+	}
+});
+
+connection.addEventListener('error', error => {
+	console.log('Ошибка:', error.data);
+});
+
+window.addEventListener('beforeunload', event => {	
+	connection.addEventListener('close', event => {
+		console.log('Соединение закрыто', event);
+	});
+	connection.close(1000);		
+});
