04748109358078551601f25064b49485b3c5735e
Roman <titangro@users.noreply.github.com>
2018-11-23 23:23:55 +0300

    Update food

--- cors/food/js/food.js
+++ cors/food/js/food.js
@@ -1,31 +1,40 @@
 'use strict';
 
-function loadData(url) {
-	const functionName = 'parser1';
-	return loadPromise(functionName, url);
+Promise.all(
+	[loadData('https://neto-api.herokuapp.com/food/42'),
+	loadRating('https://neto-api.herokuapp.com/food/42/rating'),
+	loadConsumers('https://neto-api.herokuapp.com/food/42/consumers')])
+	.then((data) => {
+		showRecipe(data[0]);
+		showRating(data[1]);
+		showConsumers(data[2]);
+	})
+	.catch((error) => {console.log(error)})
+
+function loadData(url) {	
+	return loadPromise(url);
 }
 
-function loadRating(url) {
-	const functionName = 'parser2';
-	return loadPromise(functionName, url);
+function loadRating(url) {	
+	return loadPromise(url);
 }
 
-function loadConsumers(url) {
-	const functionName = 'parser3';
-	return loadPromise(functionName, url);	
+function loadConsumers(url) {	
+	return loadPromise(url);	
 }
 
-function loadPromise(functionName, url) {	
+function loadPromise(url) {
+	const functionName = randName();
 	return new Promise((done,fail) => {
 		window[functionName] = done;	
 
-		const script = document.creatElemet('script');
+		const script = document.createElement('script');
 		script.src = `${url}?callback=${functionName}`;
+		document.body.appendChild(script);
 	});
 }
 
-function showRecipe(recipe) {
-	//document.querySelector('*[data-username]').textContent = recipe.id;
+function showRecipe(recipe) {	
 	document.querySelector('*[data-title]').textContent = recipe.title;
 	document.querySelector('*[data-title]').dataset.title = recipe.title;
 	const ingredients = document.querySelector('*[data-ingredients]');
@@ -38,13 +47,7 @@ function showRecipe(recipe) {
 	}
 	ingredients.dataset.ingredients = recipe.ingredients;
 	document.querySelector('*[data-pic]').dataset.pic = recipe.pic;
-	document.querySelector('*[data-pic]').style.backgroundImage = `url(${recipe.pic})`;	
-
-	loadRating('https://neto-api.herokuapp.com/food/42/rating')
-	.then(showRating(newParser2()))
-	.then(loadConsumers('https://neto-api.herokuapp.com/food/42/consumers')
-	.then(showConsumers(newParser3()))
-	.catch(e => {console.log(e)}))	
+	document.querySelector('*[data-pic]').style.backgroundImage = `url(${recipe.pic})`;			
 }
 
 function showRating(rating) {
@@ -59,9 +62,6 @@ function showRating(rating) {
 }
 
 function showConsumers(consumers) {
-	//consumers.name
-	//consumers.pic
-	console.log(consumers)
 	for (const item of consumers.consumers) {
 		let img = `<img src="${item.pic}" title="${item.name}">`
 		document.querySelector('*[data-consumers]').innerHTML += img;
@@ -70,16 +70,11 @@ function showConsumers(consumers) {
 	document.querySelector('*[data-consumers]').innerHTML += `<span>(+${others})</span>`;	
 }
 
-loadData('https://neto-api.herokuapp.com/food/42')
-	.then(showRecipe(newParser1()))
-	.catch((error) => {console.log(error)})
-
-function newParser1() {
-	return JSON.parse(`{"id":42,"title":"Макарун","pic":"https://neto-api.herokuapp.com/hj/4.1/food/product.jpg","ingredients":["cахар","яичные белки","масло","соль","корица"]}`);
-}
-function newParser2() {
-	return JSON.parse(`{"rating":9.1,"votes":320}`);
-}
-function newParser3() {
-	return JSON.parse(`{"consumers":[{"name":"Аманда","pic":"https://neto-api.herokuapp.com/hj/4.1/food/user1.jpg"},{"name":"Виктория","pic":"https://neto-api.herokuapp.com/hj/4.1/food/user2.jpg"},{"name":"Надежда","pic":"https://neto-api.herokuapp.com/hj/4.1/food/user3.jpg"},{"name":"Михаил","pic":"https://neto-api.herokuapp.com/hj/4.1/food/user4.jpg"}],"total":23}`);	
+function randName() {
+	let str = '';
+	let string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+	for (let i = 0; i < 7; i++) {
+		str += string.charAt(Math.floor(Math.random() * string.length))
+	}
+	return str;
 }
