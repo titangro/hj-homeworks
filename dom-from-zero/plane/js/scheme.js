09685053003eb3a767292c441261eeb8e8c5b2d5
Roman <titangro@users.noreply.github.com>
2018-11-30 02:00:42 +0300

    Update scheme

--- dom-from-zero/plane/js/scheme.js
+++ dom-from-zero/plane/js/scheme.js
@@ -1,1 +1,24 @@
 'use strict'
+
+const form = document.querySelector('.form-inline');
+const btnSeatMap = document.querySelector('.btnSeatMap');
+const btnSetFull = document.querySelector('.btnSetFull');
+const btnSetEmpty = document.querySelector('.btnSetEmpty');
+const seatMapTitle = document.querySelector('.seatMapTitle');
+
+document.addEventListener('DOMContentLoaded', requestLocation)
+
+function showId() {
+	const formData = new FormData(form);
+	for (const [k, v] of formData) {
+		return v;
+	}
+}
+
+function requestLocation() {
+	fetch(`https://neto-api.herokuapp.com/plane/${showId()}`)
+		.then((res) => { 
+			return res.json();
+		})
+		.then((res) => console.log(res))
+}
