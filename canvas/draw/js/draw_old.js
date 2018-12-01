e5d08df8e78869215e14fd325db4b10426a89e67
Roman <titangro@users.noreply.github.com>
2018-12-01 17:42:51 +0300

    Update draw + old(point) version

--- canvas/draw/js/draw_old.js
+++ canvas/draw/js/draw_old.js
@@ -6,28 +6,30 @@ const ctx = draw.getContext('2d');
 window.addEventListener('load', setSizes);
 window.addEventListener('resize', setSizes);
 
-let points = [];
+let curves = [];
 let drawing = false;
 let needsRepaint = false;
 let radius = 100;
 let hue = 0;
 
 draw.addEventListener('dblclick', event => {
-	points = [];
+	curves = [];
 	needsRepaint = true;
 	tick();
 })
 draw.addEventListener('mousedown', event => {
-	drawing = true;
-	//points.push(pushPoint(event.offsetX, event.offsetY, hue, radius));
-  	needsRepaint = true;  	
+	drawing = true;	
+	const curve = []; // create a new curve
+	curve.push(pushPoint(event.offsetX, event.offsetY, hue, radius)); // add a new point
+	curves.push(curve); // add the curve to the array of curves
+  	needsRepaint = true;
 })
 draw.addEventListener('mouseup', event => {drawing = false;})
 draw.addEventListener('mouseleave', event => {drawing = false;})
 draw.addEventListener('mousemove', event => {
 	if (drawing) {
-    	const point = pushPoint(event.offsetX, event.offsetY, hue, radius);
-    	points.push(point);    	    	
+    	const point = pushPoint(event.offsetX, event.offsetY, hue, radius)
+    	curves[curves.length - 1].push(point);
     	needsRepaint = true;
 
     	if (hue !== 360) {
@@ -52,51 +54,51 @@ draw.addEventListener('mousemove', event => {
 function setSizes(event) {	
 	draw.width = document.documentElement.clientWidth;
 	draw.height = document.documentElement.clientHeight;
-	points = [];
+	curves = [];
 }
 
 function pushPoint(x, y, hue, r) {
 	return [x, y, hue, r];
 }
 
-function circle(point) {
-  	ctx.beginPath(); 
-  	ctx.fillStyle = `hsl(${point[2]},100%,50%)`; 		
+function addCurve(points) {
+  ctx.beginPath(); 
+  ctx.lineWidth = points[0][3];
+  ctx.lineJoin = 'round';
+  ctx.lineCap = 'round';  	  
+
+  ctx.moveTo(points[0][0],points[0][1]);
+
+  for(let i = 1; i < points.length - 1; i++) {
+  	ctx.lineWidth = points[i][3];  	
+    smoothCurveBetween([points[i][0],points[i][1]],
+    					[points[i + 1][0],points[i + 1][1]]);  
+  }  
+  ctx.stroke();  
+}
+
+function circle(point) {	
+  	ctx.beginPath();  		
   	ctx.arc(point[0], point[1], point[3]/2, 0, 2 * Math.PI);
   	ctx.fill();
+  	ctx.stroke();
+  	ctx.fillStyle = `hsl(${point[2]},100%,50%)`;
+  	ctx.strokeStyle = `hsl(${point[2]},100%,50%)`;
   	ctx.closePath();  	
 }
 
-function addCurve(points1, points2) {
-	if (points2) {
-	  ctx.beginPath(); 
-	  ctx.lineWidth = points1[3];
-	  ctx.lineJoin = 'round';
-	  ctx.lineCap = 'round'; 
-	  ctx.strokeStyle = `hsl(${points1[2]},100%,50%)`;  	  
-
-	  ctx.moveTo(points2[0],points2[1]);
-
-	  smoothCurveBetween([points1[0],points1[1]],
-	    					[points2[0],points2[1]]);
-	  ctx.stroke();  
-	}
-}
-
 function smoothCurveBetween(point1, point2) {
-	ctx.quadraticCurveTo(...point1, ...point2);	
+	const control = point1.map((coord, idx) => (coord + point2[idx]) / 2);
+	ctx.quadraticCurveTo(...point1, ...control);	
 }
 
 function repaint () {
   	ctx.clearRect(0, 0, draw.width, draw.height);
-  	let i = 1;
-  	points
-    	.forEach((point) => {
-      	circle(point);       	
-      	if (points && points.length > 1) {
-      		addCurve(points[i-1], points[i]);
-      	}  
-      	i++;	    	
+
+  	curves
+    	.forEach((curve) => {
+      	circle(curve[0]);
+      	addCurve(curve);      	
     });
 }
 
