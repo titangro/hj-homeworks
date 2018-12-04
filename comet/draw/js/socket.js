7cebf0538b2e8bd357c408ec853281b6bca1919a
Roman <titangro@users.noreply.github.com>
2018-12-04 23:56:21 +0300

    Выполнено задание Коллективное рисование

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const drawSocket = new WebSocket('wss://neto-api.herokuapp.com/draw');
+
+drawSocket.addEventListener('open', event => {
+	editor.addEventListener('update', updateDraw);
+});
+
+drawSocket.addEventListener('close', event => {
+	editor.removeEventListener('update', updateDraw);
+});
+
+drawSocket.addEventListener('error', error => {
+	console.log(error)
+});
+
+function updateDraw({ canvas }) {
+	canvas.toBlob(function(blob) {		
+	  	drawSocket.send(blob);
+	});
+}
+
+   
